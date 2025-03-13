e2d72b3b3aa12e8900ab0ece941352f8b73f1fe6
lilsaad <saadinbusiness3@gmail.com>
2025-03-13 15:26:54 +0530

    Removed secret package from Git tracking

--- app/src/main/java/com/example/coinmandi/core/sensitive/GekoRoutes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.coinmandi.core.sensitive
-
-object GekoRoutes {
-    val baseurl = "https://api.coingecko.com/api/v3/"
-    val getcoinsDataEP = "coins/markets"
-    val apikey = "x-cg-demo-api-key"
-    val keyvalue = "CG-fS3NsSwKbwk9r7dWnsEUjFUx"
-}
